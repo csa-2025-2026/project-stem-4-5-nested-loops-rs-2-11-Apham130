6aa8bc15cf7cf48329f8cd556a2158b52cf01078
Apham130 <andypham4747@gmail.com>
2025-10-27 19:21:08 +0000

    all but 5

--- Main.java
+++ Main.java
@@ -5,7 +5,14 @@ public class Main
   public static void main(String[] args)
   {
     // Make sure to test your methods here
-    printNums();
+    // printNTimes("hello", 5);
+    // printNums(10, 1);
+    // uprightNumberTriangle(5);
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter an int: ");
+    int input = sc.nextInt();
+    starTree(input);
+
   }
 
   public static void printNTimes(String word, int N)
@@ -20,10 +27,10 @@ public class Main
     }
   }
 
-  public static void printNums()
+  public static void printNums(int S, int E)
   {
     // code solution here
-    for (int i = 10; i > 0; i--) {
+    for (int i = S; i >= E; i--) {
       for (int j = i; j > 0; j--) {
         System.out.print(i + " ");
       }
@@ -33,16 +40,38 @@ public class Main
 
   public static void uprightNumberTriangle(int N)
   {
-    // code solution here
+    for (int i = 1; i <= N; i++) {
+      for (int j = 1; j <= i; j++) {
+        System.out.print(j + " ");
+      }
+      System.out.println();
+    }
   }
 
-  public static void starTree()
+  public static void starTree(int N)
   {
-    // code solution here
+    int j = 0;
+    int y = N;
+    for (int x = 0; x < N; x++) {
+      for (int k = 0; k < j; k++) {
+          System.out.print(" ");
+        }
+      for (int i = y; i > 0; i--) { 
+        System.out.print("* ");
+      }
+      System.out.println();
+      j++;
+      y--;
+    }
   }
 
   public static void multTable()
   {
-    // code solution here
+    int j = 1;
+    for (int i = 1; i <= 10; i++) {
+      
+      
+      j++
+    }
   }
 }
