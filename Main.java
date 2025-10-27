f5cf1076551eddfe7e4f72210ca3899cdd25f413
Apham130 <andypham4747@gmail.com>
2025-10-27 18:22:43 +0000

    1 and 2 done

--- Main.java
+++ Main.java
@@ -5,16 +5,30 @@ public class Main
   public static void main(String[] args)
   {
     // Make sure to test your methods here
+    printNums();
   }
 
   public static void printNTimes(String word, int N)
   {
     // code solution here
+    for (int i = 0; i < word.length(); i++) {
+      String let = word.substring(i,i+1);
+      for (int j = 0; j < N; j++) {
+        System.out.print(let + " ");
+      }
+      System.out.println();
+    }
   }
 
   public static void printNums()
   {
     // code solution here
+    for (int i = 10; i > 0; i--) {
+      for (int j = i; j > 0; j--) {
+        System.out.print(i + " ");
+      }
+      System.out.println();
+    }
   }
 
   public static void uprightNumberTriangle(int N)
